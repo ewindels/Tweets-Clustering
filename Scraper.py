c86d7e0ace6ff9b9273c9bd1148d3aa953a73eb8
Etienne Windels <etienne.windels@hec.edu>
2019-09-23 20:02:46 +0200

    created scraper python file

---
+++ Scraper.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import twitter
+import re
+import json
+
+with open('credentials.json') as handle:
+    credentials = json.loads(handle.read())
+
+api = twitter.Api(consumer_key=credentials['consumer_key'],
+                  consumer_secret=credentials['consumer_secret'],
+                  access_token_key=credentials['access_token_key'],
+                  access_token_secret=credentials['access_token_secret'],
+                  tweet_mode='extended')
+
+with open('companies.json') as handle:
+    companies = json.loads(handle.read())
+
+
+def get_tweets(company, id_=0, mode='newest'):
+    company_account = companies[company]
+    query = '%40{}%20-filter%3Aretweets%20-filter%3Areplies'.format(company_account)
+    count = 100
+
+    if mode == 'oldest':
+        return api.GetSearch(raw_query="q={}&max_id={}&lang=fr&count={}".format(query, id_, count),
+                             result_type='recent')
+    elif mode == 'newest':
+        return api.GetSearch(raw_query="q={}&since_id={}&lang=fr&count={}".format(query, id_, count),
+                             result_type='recent')
+    elif mode == 'new_company':
+        return api.GetSearch(raw_query="q={}&lang=fr&count={}".format(query, count), result_type='recent')
+
+
+def filter_img(text):
+    return re.sub(r'https://t.co/\w+', '[LIEN]', text)
+
+
+def create_new_data(tweets):
+    new_data = pd.DataFrame()
+    new_data['id'] = [tweet.id for tweet in tweets]
+    new_data['text'] = [filter_img(tweet.full_text) for tweet in tweets]
+    new_data['data'] = [tweet.created_at for tweet in tweets]
+    new_data['user'] = [tweet.user.name for tweet in tweets]
+    new_data['user_id'] = [tweet.user.id for tweet in tweets]
+    new_data['favorite_cnt'] = [tweet.favorite_count for tweet in tweets]
+    new_data['retweet_cnt'] = [tweet.retweet_count for tweet in tweets]
+    return new_data
+
+
+def update_data(company, mode='newest'):
+    """
+    Supported modes are newest, oldest, and new_company
+    """
+    if company in companies:
+        data_df = pd.read_csv('data/{}.csv'.format(company))
+    else:
+        mode = 'new_company'
+        data_df = pd.DataFrame(columns=['id', 'text', 'data', 'user', 'user_id', 'favorite_cnt', 'retweet_cnt'])
+    print('{:>5} tweets for {:<15}'.format(data_df.shape[0], company), end=' | ')
+
+    if mode == 'newest':
+        new_tweets = list()
+        id_ = data_df['id'].max() + 1
+        while True:
+            tweets = get_tweets(company, id_, mode)
+            tweets_filter = [tweet for tweet in tweets if (company.lower() not in tweet.user.name.lower())]
+            if len(tweets_filter) > 0:
+                new_tweets.extend(tweets_filter)
+                id_ = max([tweet.id for tweet in tweets]) + 1
+            else:
+                break
+    elif mode == 'oldest':
+        new_tweets = list()
+        id_ = data_df['id'].min() - 1
+        while True:
+            tweets = get_tweets(company, id_, mode)
+            tweets_filter = [tweet for tweet in tweets if (company.lower() not in tweet.user.name.lower())]
+            if len(tweets_filter) > 0:
+                new_tweets.extend(tweets_filter)
+                id_ = min([tweet.id for tweet in tweets]) - 1
+            else:
+                break
+    elif mode == 'new_company':
+        id_ = 0
+        tweets = get_tweets(company, id_, mode)
+        new_tweets = [tweet for tweet in tweets if (company.lower() not in tweet.user.name.lower())]
+        id_ = min([tweet.id for tweet in tweets]) - 1
+        while True:
+            mode = 'oldest'
+            tweets = get_tweets(company, id_, mode)
+            tweets_filter = [tweet for tweet in tweets if (company.lower() not in tweet.user.name.lower())]
+            if len(tweets_filter) > 0:
+                new_tweets.extend(tweets_filter)
+                id_ = min([tweet.id for tweet in tweets]) - 1
+            else:
+                break
+    else:
+        print('Mode does not exist')
+        return 1
+    new_data = create_new_data(new_tweets)
+    print('{:>4} new tweets'.format(len(new_tweets)))
+
+    if new_data.shape[0] > 1:
+        new_data_df = pd.concat([data_df, new_data])
+        new_data_df.to_csv('data/{}.csv'.format(company), index=False)
+        return 0
+    else:
+        return 1
+
+
+def update_all():
+    for company in sorted(companies):
+        update_data(company, 'newest')
+
