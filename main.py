5d7e3f0b0cfdd6935dd7adaa2521d79d3fb0321f
Etienne Windels <etienne.windels@hec.edu>
2019-10-02 16:27:24 +0200

    added CLI options for adding company

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Scraper import update_all
+from Scraper import update_all, add_company
 import argparse
 import sys
 
@@ -8,11 +8,19 @@ my_parser.add_argument('--mode',
                        type=str,
                        help='retrieve tweets from companies or to companies')
 
+my_parser.add_argument('--add',
+                       type=str,
+                       help='add new company')
+
 args = my_parser.parse_args()
 input_mode = args.mode
+new_company = args.add
 
-if input_mode not in ['from', 'to']:
-    print("The mode specified does not exist (only 'to' and 'from' are supported)")
-    sys.exit()
-
-update_all(input_mode)
+if input_mode:
+    if input_mode not in ['from', 'to']:
+        print("The mode specified does not exist (only 'to' and 'from' are supported)")
+        sys.exit()
+    else:
+        update_all(input_mode)
+if new_company:
+    add_company(new_company)
